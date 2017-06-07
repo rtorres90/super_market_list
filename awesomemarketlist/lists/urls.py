c15b07ddacb01d62a7193096d48d62f998f73b82
Don Roberto Torres <roberto.torresdn@gmail.com>
2017-06-07 13:15:59 +0000

    LIST creation without logic for option objects.

---
+++ awesomemarketlist/lists/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'lists'
+
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^new/$', views.new_list, name='new_list'),
+    url(r'^create/$', views.create_list, name='create_list'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<list_id>[0-9]+)/update/$', views.update, name='update'),
+    ]
--- awesomemarketlist/lists/views.py
+++ awesomemarketlist/lists/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.views import generic
 # Create your views here.
+
+from .models import List, Tag
+
+class IndexView(generic.ListView):
+    template_name = 'lists/index.html'
+    model = List
+    
+class DetailView(generic.DetailView):
+    model = List
+    template_name = 'lists/detail.html'
+
+def update(request, list_id):
+    list = get_object_or_404(List, pk=list_id)
+    try:
+        list.rename_list(request.POST['name'])
+        list.budget = int(request.POST['budget'])
+        list.save()
+        error_message = None
+    except:
+        error_message = "Something was wrong."
+    return render(request, 'lists/detail.html', {
+        'list': list,
+        'error_message': error_message,
+    })
+    
+def new_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'lists/new_list.html', {'tags': tags})
+    
+def create_list(request):
+    tag = Tag.objects.get(pk=request.POST['tag_id'])
+    new_list = List(name=request.POST['name'], budget=request.POST['budget'], tag=tag)
+    new_list.save()
+    
+    return HttpResponseRedirect(reverse('lists:index'))
+    
